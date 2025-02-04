9afdcc623fbae7cdb5cbf0ec91b1190a3b2a97c6
rubek <rubek@ottr.com.np>
2025-02-04 11:37:16 +0545

    🏗  Building-construction: switch to rollup bundler

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import css from "rollup-plugin-import-css";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "dist",
+    }),
+    css(),
+  ],
+  external: ["react"],
+};
